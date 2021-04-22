b4be25a99c217a9248de17352c79de78b15431d2
Robert Bright <robright28@gmail.com>
2021-04-22 05:56:12 +0000

    a basic setup for a request to understand how things works

--- auth-config.js
+++ auth-config.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 
 module.exports = {
-  clientId: process.env.UID,
-  clientSecret: process.env.SECRET,
-  accessTokenUri: 'https://api.intra.42.fr/oauth/token',
-  authorizationUri: 'https://api.intra.42.fr/oauth/authorize',
-  redirectUri: 'http://www.google.com/'
+  config: {
+    clientId: process.env.UID,
+    clientSecret: process.env.SECRET,
+    accessTokenUri: 'https://api.intra.42.fr/oauth/token',
+    authorizationUri: 'https://api.intra.42.fr/oauth/authorize',
+    redirectUri: 'http://www.google.com/'
+  },
+  auth: {
+    body: `grantType=client_credentials&client_id=${process.env.UID}&` +
+      `client_secret=${process.env.SECRET}`,
+    headers: {"content-type": 'application/x-www-form-urlencoded'},
+    base: "https://api.intra.42.fr"
+  }
 }
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const OAuth2 = require("client-oauth2");
-const opts = require("./auth-config");
+const {config, auth} = require("./auth-config");
+const path = require("path");
 
-const client = new OAuth2(opts);
+const client = new OAuth2(config);
+const fetch = (req) => {
+  const method = req.method ?? "GET";
+  const uri = path.join(auth.base, req.uri);
+  const body = auth + "&" + req.body;
+  const headers = auth.headers;
 
-async function run() {
-  const token = await client.credentials.getToken();
-
-  console.log(client.request.toString());
+  if (req.token?.tokenType === 'bearer') {
+    headers.Authorization = `Bearer ${req.token.accessToken}`;
+  }
+  return client.request(
+    method,
+    uri,
+    body,
+    headers
+  );
 }
 
-run().then(() => console.log("done!"))
+async function main() {
+  const token = await client.credentials.getToken();
+  // console.log(token);
+  // console.log(client.request.toString());
+  fetch({uri: "/v2/users/fokrober", token}).then((res) => console.log(res))
+}
+
+main().then(() => console.log("done!"))
